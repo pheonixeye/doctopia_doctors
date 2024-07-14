9b05267b30ae4a714afeed885623d894778f54b5
pheonixeye <drkazgm@gmail.com>
2024-07-14 19:31:28 +0300

    scrapper issue resolved

--- lib/api/scrapper_api/scrapper_api.dart
+++ lib/api/scrapper_api/scrapper_api.dart
@@ -1,37 +1,15 @@
-import 'package:chaleno/chaleno.dart';
+import 'package:doctopia_doctors/api/_pocket_main/pocket_main.dart';
 import 'package:doctopia_doctors/models/med_article/med_article.dart';
-import 'package:uuid/uuid.dart';
 
 class HxScrapper {
   Future<List<MedArticle>> init(int page) async {
-    late final String url =
-        "https://www.medscape.com/index/list_13470_${page - 1}";
+    final response = await PocketbaseHelper.pb.collection("medscape").getList(
+          page: page,
+          perPage: 10,
+        );
 
-    List<Result> _results = [];
-    await Chaleno().load(url).then((p) {
-      for (var i = 1; i < 21; i++) {
-        final res = p?.querySelector("#archives > ul > li:nth-child($i)");
-        if (res != null) {
-          _results.add(res);
-        }
-      }
-    });
-    // print("${_results.map((e) => e.html).toList()}");
-    final _articles = _results.map((e) {
-      final url = e.querySelector("a")?.href;
-      final title = e.querySelector(".title")?.innerHTML;
-      final teaser = e.querySelector("span")?.text;
-      final from = e.querySelector(".byline")?.text;
-
-      return MedArticle(
-          id: const Uuid().v4(),
-          title: title ?? "",
-          teaser: teaser ?? "",
-          url: url ?? "",
-          from: from ?? "");
-    }).toList();
-
-    // print("${_articles[3]}");
+    final _articles =
+        response.items.map((e) => MedArticle.fromJson(e.toJson())).toList();
     return _articles;
   }
 }
